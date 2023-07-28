42d678fca92b466cfbc7471894ab1987e2d02fdf
pnordboj <91888307+pnordboj@users.noreply.github.com>
2023-07-28 13:38:41 +0200

    update

--- app.py
+++ app.py
@@ -5,10 +5,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 import io
+import pytube
+from pytube import YouTube
+import moviepy.editor as mp
+import os
+
+import openai
+import whisper
 
 app = Flask(__name__)
-cors = CORS(app)
+CORS(app)
+
 app.config["CORS_HEADERS"] = "Content-Type"
+openai.api_key = "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"
 
 model = tf.keras.applications.MobileNetV2(weights="imagenet")
 
@@ -23,6 +32,7 @@ def predict():
         return "No file found"
     if file:
         image = Image.open(io.BytesIO(file.read()))
+        image = image.convert("RGB")
         image = image.resize((224, 224))
         image = tf.keras.preprocessing.image.img_to_array(image)
         image = tf.keras.applications.mobilenet_v2.preprocess_input(image)
@@ -40,6 +50,32 @@ def predict():
             ]
         }
         return response
+
+
+@app.route("/speechtotext", methods=["POST"])
+@cross_origin()
+def speechtotext():
+    file = request.files.get("file")
+    youtube_url = request.form.get("youtube_url")
+    if not file and not youtube_url:
+        return "No file or YouTube URL provided"
+    if youtube_url:
+        yt = YouTube(youtube_url)
+        audio = yt.streams.get_audio_only()
+        videos_dir = "./videos"
+        result = audio.download(output_path=videos_dir)
+    elif file.filename == "":
+        return "No file found"
+
+    print("Sending audio for transcription...")
+
+    model = whisper.load_model("medium.en")
+    transcript = model.transcribe(result)
+    print("Transcription complete.")
+
+    print("Result: " + transcript["text"])
+
+    return {"transcript": transcript["text"]}
 
 
 if __name__ == "__main__":
